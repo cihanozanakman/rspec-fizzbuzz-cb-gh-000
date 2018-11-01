ed4fe550124e3dff711b52aa87b408539915888e
Cihan Akman <cihanozanakman@gmail.com>
2018-11-01 20:13:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,4 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(value)
   string = ""
+  if value % 3
+    string += "Fizz"
+  end
+  if value % 5
+    string += "Fizz"
+  end
 end
